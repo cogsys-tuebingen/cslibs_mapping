6c125d6837d1d98a1cfe046ddc712cb6a2fa8c53
Richard Hanten <richard.hanten@uni-tuebingen.de>
2018-09-11 13:45:06 +0200

    the native flag

--- cmake/cslibs_mapping_extras.cmake
+++ cmake/cslibs_mapping_extras.cmake
@@ -5,7 +5,7 @@ add_definitions(-W
                 -fno-strict-aliasing
                 -Wno-deprecated-register)
 
-if(${PROJECT_NAME}_USE_NATIVE)
+if(${${PROJECT_NAME}_USE_NATIVE} OR ${CS_USE_NATIVE})
     add_definitions(-march=native)
 endif()
 
